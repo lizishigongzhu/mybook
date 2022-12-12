e4ec5a607e249b5839c5616488641031f4601d35
liangsunyang <1499599640@qq.com>
2022-12-12 20:31:40 +0800

    测试

---
+++ src/main/java/com/mybook/controller/UserRoleController.java
@@ -0,0 +1,15 @@
+package com.mybook.controller;
+
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+
+/**
+ * 乐字节：专注线上IT培训
+ * 答疑老师微信：lezijie
+ */
+@Controller
+public class UserRoleController {
+
+
+}
---
+++ src/main/java/com/mybook/service/UserRoleService.java
@@ -0,0 +1,15 @@
+package com.mybook.service;
+
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 乐字节：专注线上IT培训
+ * 答疑老师微信：lezijie
+ */
+@Service
+public class UserRoleService  {
+
+}
