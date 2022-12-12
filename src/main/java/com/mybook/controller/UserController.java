a8ee68b0dd29dacde7e6d54f5a07f35bce1f7ed5
1094003819 <1094003819@qq.com>
2022-12-12 21:15:31 +0800

    第五次

--- src/main/java/com/mybook/controller/UserController.java
+++ src/main/java/com/mybook/controller/UserController.java
@@ -4,23 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
-
     public void test(){
-        System.out.println("哇咔咔");
-    } public void test1(){
-        System.out.println("第二次哇卡啊卡");
-    } public void test2(){
-        System.out.println("第二次哇咔咔");
-    } public void test3(){
-        System.out.println("第二次哇咔咔");
-    }
-    public void test5(){
-        System.out.println("第二次哇咔咔");
-    }
-    public void test6(){
-        System.out.println("第二次哇咔咔");
-    }  public void test7(){
-        System.out.println("第二次哇咔咔");
+        System.out.println("ggg");
     }
 }
-
